d664461bd84ebf95aa658889b7c3566f79249a97
villafanes <villafanesani@gmail.com>
2023-07-27 14:35:10 -0400

    7/27 2:35

--- Graph.h
+++ Graph.h
@@ -6,6 +6,21 @@
 
 using namespace std;
 class Graph{
-    map<string, vector<string, double>> popSongs;
+    map<string, vector<string, double>> darktrap;
+    map<string, vector<string, double>> underground;
+    map<string, vector<string, double>> trapmetal;
+    map<string, vector<string, double>> emo;
+    map<string, vector<string, double>> rap;
+    map<string, vector<string, double>> rnb;
+    map<string, vector<string, double>> pop;
+    map<string, vector<string, double>> hiphop;
+    map<string, vector<string, double>> techhouse;
+    map<string, vector<string, double>> techno;
+    map<string, vector<string, double>> trance;
+    map<string, vector<string, double>> psytrance;
+    map<string, vector<string, double>> trap;
+    map<string, vector<string, double>> dnb;
+    map<string, vector<string, double>> hardstyle;
+
 };
 #endif //PROJECT3_GRAPH_H
