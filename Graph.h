5c785ed6f2ab0dda8e704b35ec8c1066b59b0490
villafanes <117455242+villafanes@users.noreply.github.com>
2023-08-05 17:23:30 -0400

    Cleaned up

--- Graph.h
+++ Graph.h
@@ -16,8 +16,7 @@
 
 using namespace std;
 class Graph{
-    // map<string, float> genre;
-    // genre, energy level, song name
+    // maps of the songs and their energy levels by genre
     map<string, float> darktrap;
     map<string, float> undergroundrap;
     map<string, float> trapmetal;
@@ -27,6 +26,7 @@ class Graph{
     map<string, float> pop;
     map<string, float> hiphop;
 
+    // graphs connecting each song to another if their energy levels are at most .1 apart, by genre
     map<string, vector<pair<string, float>>> darktrapGraph;
     map<string, vector<pair<string, float>>> undergroundrapGraph;
     map<string, vector<pair<string, float>>> trapmetalGraph;
@@ -38,14 +38,14 @@ class Graph{
 
 public:
     void ReadFile();
-    void CreateGraph(map<string, vector<pair<string, float>>>& graph, map<string, float>& genre);
-    vector<pair<string, float>> CreateConstantPlaylist(string genre, int playlistSize, int energy); //BFS
-    vector<pair<string, float>> ConstantPlaylistHelper(map<string, vector<pair<string,float>>> &graph, map<string, float> &map, int playlistSize, int energy);
-    vector<pair<string, float>> CreateRangePlaylist(string genre, int playlistSize, int energy1, int energy2); //DFS
-    vector<pair<string, float>> RangePlaylistHelper(map<string, vector<pair<string,float>>>& graph, map<string, float>& map, int playlistSize, int energy1, int energy2);
-    map<string, vector<pair<string, float>>> CheckGraph(string genre);
-    map<string, float> CheckMap(string genre);
-    string RandomSongHelper(map<string, float> &genre, int energy);
+    void CreateGraph(map<string, vector<pair<string, float>>>& graph, map<string, float>& genre); 
+    vector<pair<string, float>> CreateConstantPlaylist(string genre, int playlistSize, int energy); // creates BFS playlist
+    vector<pair<string, float>> ConstantPlaylistHelper(map<string, vector<pair<string,float>>> &graph, map<string, float> &map, int playlistSize, int energy); // conducts BFS through given genre graph
+    vector<pair<string, float>> CreateRangePlaylist(string genre, int playlistSize, int energy1, int energy2); // creates DFS playlist
+    vector<pair<string, float>> RangePlaylistHelper(map<string, vector<pair<string,float>>>& graph, map<string, float>& map, int playlistSize, int energy1, int energy2); // conducts DFS through given genre graph
+    map<string, vector<pair<string, float>>> CheckGraph(string genre); // processes user input to create the appropriate graph
+    map<string, float> CheckMap(string genre); // processess user input to search the appropriate map
+    string RandomSongHelper(map<string, float> &genre, int energy); // generates random source vertex 
 };
 
 
@@ -68,7 +68,7 @@ void Graph::ReadFile() {
         getline(str, energy, ',');
         getline(str, name, '\n');
 
-
+        // processes data set values into their appropriate containers
         if (genre == "Dark Trap"){
             darktrap[name] = stod(energy);
         }
@@ -100,16 +100,14 @@ void Graph::ReadFile() {
 void Graph::CreateGraph(map<string, vector<pair<string, float>>> &graph, map<string, float> &genre) {
     map<string, vector<pair<string, float>>> tempGraph;
     for(auto it = genre.begin(); it != genre.end(); it++){
-        //cout << it->first;
         for(auto iter = genre.find(it->first); iter != genre.end(); iter++){
             if (it->first != iter->first){
                 float edgeWeight = abs(it->second - iter->second);
-                //cout << it->first <<  " " << iter->first <<  " " << edgeWeight << endl;
-                if(edgeWeight <= 0.10){
+                if(edgeWeight <= 0.10){ // if a song's energy is at most .1 different from another's, they are pointed to each other
                     graph[it->first].push_back(make_pair(iter->first, edgeWeight));
                     graph[iter->first].push_back(make_pair(it->first, edgeWeight));
                 }
-                else { //make sure all songs are graphed even if they are disconnected
+                else { // ensures all songs are graphed even if they are disconnected
                     if (graph.find(it->first) == graph.end()){
                         graph[it->first] = {};
                     }
@@ -120,6 +118,7 @@ void Graph::CreateGraph(map<string, vector<pair<string, float>>> &graph, map<str
 }
 
 map<string, vector<pair<string, float>>> Graph ::CheckGraph(string genre){
+    // check's user input before creating the graph
     if (genre == "Dark Trap"){
         CreateGraph(darktrapGraph, darktrap);
         return darktrapGraph;
@@ -188,18 +187,18 @@ string Graph::RandomSongHelper(map<string, float> &genre, int energy) {
         min = 90;
         max = 100;
     } else {
-        energy = energy * 10; // changes energy from 3 to 30, for example
+        energy = energy * 10; 
         min = energy;
-        max = energy + 9;
+        max = energy + 9; // if a user enters "3", a range of energy levels is created from 30-39 (3.0-3.9) that the source vertex can be chosen from
     }
 
     bool found = false;
 
     while (!found){;
-        double source_energy = (rand() % (max - min + 1)) + min;
+        double source_energy = (rand() % (max - min + 1)) + min; 
         for(auto it = genre.begin(); it != genre.end(); it++) {
             if (ceil(it->second * 100) == source_energy) {
-                //cout << "found" << endl;
+                // checks if the random value generated exists in the genre's graph
                 found = true;
                 return it->first;
             }
@@ -208,20 +207,19 @@ string Graph::RandomSongHelper(map<string, float> &genre, int energy) {
             }
         }
     }
-//    cout << "done" << endl;
 }
 
 vector<pair<string, float>> Graph::ConstantPlaylistHelper(map<string, vector<pair<string,float>>>& graph, map<string, float>& map, int playlistSize, int energy) {
-    //BFS
-    string startSong = RandomSongHelper(map, energy); //find random song that matches energy level
-    vector<pair<string, float>> playlist; // initiate playlist of songs
-    set<string> visited; // set to check if vertex has already been visited
-    queue<string> q; //queue for vertexes
+    string startSong = RandomSongHelper(map, energy); // find random song that matches energy level
+    vector<pair<string, float>> playlist; // initiate playlist container for songs
+    set<string> visited; // stores the verticies that have been visited 
+    queue<string> q; // queue of verticies
 
     visited.insert(startSong);
     q.push(startSong);
     playlist.push_back(make_pair(q.front(), map[q.front()] * 10));
 
+    // BFS
     while (playlist.size() != playlistSize) { //continue until we reach desired playlist size
         string u = q.front();
         q.pop();
@@ -242,31 +240,32 @@ vector<pair<string, float>> Graph::ConstantPlaylistHelper(map<string, vector<pai
 }
 
 vector<pair<string, float>> Graph::RangePlaylistHelper(map<string, vector<pair<string,float>>>& graph, map<string, float>& map, int playlistSize, int energy1, int energy2) {
-    string startSong = RandomSongHelper(map, energy1);
-    vector<pair<string, float>> playlist;
-    set<string> visited;
-    set<float> visitedVal;
-    stack<string> s;
+    string startSong = RandomSongHelper(map, energy1); // find random song that matches energy level
+    vector<pair<string, float>> playlist; // initiate playlist container for songs
+    set<string> visited; // stores the verticies that have been visited 
+    set<float> visitedVal; // stores the energy levels already in the playlist to ensure a variety of levels
+    stack<string> s; // stack of verticies
 
     visited.insert(startSong);
     s.push(startSong);
-    playlist.emplace_back(s.top(), (map[s.top()]) * 10.0);
+    playlist.emplace_back(s.top(), (map[s.top()]) * 10.0); // convert's the energy level in the data set to match the visual's energy levels (ex. 0.38 becomes 3.8) 
 
+    // DFS
     while((playlist.size() != playlistSize) && !s.empty()) {
         string u = s.top();
-        if ((u != startSong) && visitedVal.count(map[u] * 10.0) == 0){
+        if ((u != startSong) && visitedVal.count(map[u] * 10.0) == 0){ // skips re-entering the source node and any energy levels already in the graph
             visitedVal.insert(map[u] * 10.0);
             playlist.emplace_back(u, (map[u] * 10.0));
         }
         
         s.pop();
-        vector<pair<string, float>> neighbors = graph[u];
-        if (!neighbors.empty()){
+        vector<pair<string, float>> neighbors = graph[u]; 
+        if (!neighbors.empty()){ // visits the neighbors of the vertex on the top of the stack
             for (auto it = neighbors.begin(); it != neighbors.end(); it++) {
                 if ((visited.count(it->first) == 0)) {
                     visited.insert(it->first);
                     if (((map[it->first] * 10.0) >= energy1) && ((map[it->first] * 10.0) <= energy2)) {
-                        s.push(it->first);
+                        s.push(it->first); // pushes all values within the range given by the user onto the stack
                     }
                 }
                 if (playlist.size() == playlistSize) {
@@ -279,7 +278,7 @@ vector<pair<string, float>> Graph::RangePlaylistHelper(map<string, vector<pair<s
 }
 
 vector<pair<string, float>> Graph::CreateConstantPlaylist(string genre, int playlistSize, int energy) {
-    map<string, vector<pair<string,float>>> genreGraph = CheckGraph(genre);
+    map<string, vector<pair<string,float>>> genreGraph = CheckGraph(genre); 
     map<string, float> genreMap = CheckMap(genre);
     return ConstantPlaylistHelper(genreGraph, genreMap, playlistSize, energy);
 }
@@ -289,4 +288,5 @@ vector<pair<string, float>> Graph::CreateRangePlaylist(string genre, int playlis
     map<string, float> genreMap = CheckMap(genre);
     return RangePlaylistHelper(genreGraph, genreMap, playlistSize, energy1, energy2);
 }
+
 #endif //PROJECT3_GRAPH_H
